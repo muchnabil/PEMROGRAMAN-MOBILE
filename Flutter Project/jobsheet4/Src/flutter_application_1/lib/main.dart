82dbb502dbc9c935de538a93f1ec986fca272be5
muchnabil <muchammadnabil@gmail.com>
2026-04-19 01:55:57 +0700

    Merge semua tugas praktikum ke satu repository

---
+++ Flutter Project/jobsheet4/Src/flutter_application_1/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Laporan Praktikum 3')),
+        body: const Center(
+          child: Text(
+            'Muchammad Nabil', // GANTI DENGAN NAMA LENGKAPMU
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
