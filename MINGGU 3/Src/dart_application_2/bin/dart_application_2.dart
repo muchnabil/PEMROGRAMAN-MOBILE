fc79b8c4ceb9eb4690506b5f99c967d4cf8d047f
muchnabil <muchammadnabil@gmail.com>
2026-04-24 08:39:59 +0700

    Commit 2: Menambahkan fungsi pengecekan bilangan prima

--- MINGGU 3/Src/dart_application_2/bin/dart_application_2.dart
+++ MINGGU 3/Src/dart_application_2/bin/dart_application_2.dart
@@ -1,4 +1,12 @@
 import 'package:dart_application_2/dart_application_2.dart'
     as dart_application_2;
 
+bool isPrima(int angka) {
+  if (angka <= 1) return false;
+  for (int i = 2; i <= angka ~/ 2; i++) {
+    if (angka % i == 0) return false;
+  }
+  return true;
+}
+
 void main() {}
