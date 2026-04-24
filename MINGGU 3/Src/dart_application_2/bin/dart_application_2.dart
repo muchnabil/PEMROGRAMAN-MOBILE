dfd69973407ab1f9444d0d6275b4aa63989b40f0
muchnabil <muchammadnabil@gmail.com>
2026-04-24 08:43:43 +0700

    Commit 3: Implementasi perulangan angka 1 sampai 20

--- MINGGU 3/Src/dart_application_2/bin/dart_application_2.dart
+++ MINGGU 3/Src/dart_application_2/bin/dart_application_2.dart
@@ -9,4 +9,10 @@ bool isPrima(int angka) {
   return true;
 }
 
-void main() {}
+void main() {
+  for (int i = 1; i <= 20; i++) {
+    if (isPrima(i)) {
+      print("$i adalah bilangan prima");
+    }
+  }
+}
